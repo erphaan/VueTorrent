99a41eabcae18ed57f7f57fe38c353b412aa0d77
usmanmughalji <35332218+usmanmughalji@users.noreply.github.com>
2019-08-16 15:41:45 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -108,7 +108,9 @@ app.delete('/upload', upload.single('file'), (req, res) => {
   return res.send('deleted file')
 })
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 app.get('/test', (req, res) => {
